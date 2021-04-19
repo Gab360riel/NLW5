ac88c11485e8262b6e86b4b2e6d5f54412a38689
Gab360riel <gabrield_martins@hotmail.com>
2021-04-19 17:10:43 -0300

    primeira aula

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express()
+
+app.get("/", (req, res) => {
+  return res.json({response: "Hello World"})
+})
+
+app.post("/", (req, res) => {
+  return res.json({response: "Usuário salvo com sucesso!"})
+})
+
+app.listen(3000, () => console.log("O servidor está rodando na porta 3000"))
