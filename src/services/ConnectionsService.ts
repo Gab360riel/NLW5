206e902bcaa106af56227cd0f4124917027732fe
Gab360riel <gabrield_martins@hotmail.com>
2021-04-23 19:46:22 -0300

    Finalização NLW 5

--- src/services/ConnectionsService.ts
+++ src/services/ConnectionsService.ts
@@ -40,6 +40,33 @@ class ConnectionService {
         return user;
     }
 
+    async indexAdmin() {
+        const connections = await this.ConnectionsRepository.find({
+            where: {admin_id: null},
+            relations: ["user"],
+        })
+
+        return connections;
+    }
+
+    async indexSocketID(socket_id: string) {
+        const connection = await this.ConnectionsRepository.findOne({
+            socket_id,
+        });
+
+        return connection;
+    }
+
+    async updateAdminId(user_id: string, admin_id: string) {
+        await this.ConnectionsRepository
+            .createQueryBuilder()
+            .update(Connection)
+            .set({admin_id})
+            .where("user_id = :user_id", {
+                user_id,
+            }).execute();
+    }
+
 };
 
 export { ConnectionService };
